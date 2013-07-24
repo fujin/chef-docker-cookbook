0f0f3c8f12d67a24de44094fd5b5faa6bdfe4bcb
AJ Christensen <aj@junglist.io>
2013-07-24 16:05:18 +1200

    [docker] special case the kernel extra modules image name on virtual guests

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -1,5 +1,12 @@
+kernel_release_version =
+  if node['virtualization']['role'] == 'guest'
+    node['kernel']['release'].gsub('generic', 'virtual')
+  else
+    node['kernel']['release']
+  end
+
 default['docker']['kernel_modules_package'] =
-  ['linux-image-extra', node['kernel']['release']].join('-')
+  ['linux-image-extra', kernel_release_version].join('-')
 
 default['docker']['apt_repository_uri'] =
   'http://ppa.launchpad.net/dotcloud/lxc-docker/ubuntu'
