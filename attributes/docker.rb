a5be8207c4ae3e2c1ae75ae4b00d7acda0654b6f
AJ Christensen <aj@junglist.io>
2013-07-24 15:59:20 +1200

    [docker] join the kernel with "-"

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -1,5 +1,5 @@
 default['docker']['kernel_modules_package'] =
-  ['linux-image-extra', node['kernel']['release']].join
+  ['linux-image-extra', node['kernel']['release']].join('-')
 
 default['docker']['apt_repository_uri'] =
   'http://ppa.launchpad.net/dotcloud/lxc-docker/ubuntu'
