355ef9ef7eef05ecfe00da65a409b669146ec1c4
AJ Christensen <aj@junglist.io>
2013-07-24 15:46:19 +1200

    [docker] initial commit, minimal docker cb

---
+++ attributes/docker.rb
@@ -0,0 +1,5 @@
+default['docker']['kernel_modules_package'] =
+  ['linux-image-extra', node['kernel']['release']].join
+
+default['docker']['apt_repository_uri'] =
+  'http://ppa.launchpad.net/dotcloud/lxc-docker/ubuntu'
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'docker'
+maintainer       'AJ Christensen'
+maintainer_email 'aj@junglist.io'
+license          'Apache 2.0'
+description      'Installs/Configures docker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'ubuntu'
+
+depends 'ubuntu'
+depends 'apt'
